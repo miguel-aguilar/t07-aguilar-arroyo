43ab73503a9aa0c688727d8948fa324286056a42
miguel aguilar <maguilarv@unprg.edu.pe>
2019-11-22 20:56:39 -0500

    se agrego el lote.err.bat con textos invalidos sen ha unido el archivo de mi compañero y el mio juntos

---
+++ lote.err.bat
@@ -0,0 +1,39 @@
+python aguilar/mientras/ejercicio01.py "Miguel" "reunion familiar" febrero
+
+python aguilar/mientras/ejercicio02.py "Mauricio" "ing. electronica" 180
+
+python aguilar/mientras/ejercicio03.py "Juan" "manzanas" 934 4.5
+
+python aguilar/mientras/ejercicio04.py "miguel_av" "maguilarv@unprg.edu.pe" "12344" 167
+
+python aguilar/mientras/ejercicio05.py "Coraima" "Miguel" martes "quisieras salir conmigo?"
+
+python aguilar/para/ejercicio06.py treinta
+
+python aguilar/para/ejercicio07.py veinticinco
+
+python aguilar/para/ejercicio08.py doce
+
+python aguilar/para/ejercicio09.py ochenta
+
+python aguilar/para/ejercicio10.py noventa
+
+python aguilar/iteracion/ejercicio11.py "GYM"
+
+python aguilar/iteracion/ejercicio12.py "FE"
+
+python aguilar/iteracion/ejercicio13.py "ABC"
+
+python aguilar/iteracion/ejercicio14.py "HIRU"
+
+python aguilar/iteracion/ejercicio15.py "QUEQUE"
+
+python aguilar/iteracion_en_rango/ejercicio16.py 20 2 2
+
+python aguilar/iteracion_en_rango/ejercicio17.py 35 1
+
+python aguilar/iteracion_en_rango/ejercicio18.py 56 3 4
+
+python aguilar/iteracion_en_rango/ejercicio19.py 21 4
+
+python aguilar/iteracion_en_rango/ejercicio20.py 29 3
