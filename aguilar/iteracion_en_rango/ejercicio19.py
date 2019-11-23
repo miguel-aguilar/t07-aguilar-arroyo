0361d1063ab7701cab05e44b460599516efb9f2c
miguel aguilar <maguilarv@unprg.edu.pe>
2019-11-22 19:17:23 -0500

    se agrego el ejercicio 19 tipo ITERACION EN RANGO

---
+++ aguilar/iteracion_en_rango/ejercicio19.py
@@ -0,0 +1,18 @@
+import os
+#declaracion de variables
+x,y=0,0
+#imprimir los numeros que se encuentran  en cierto rango en un solo intervalo
+#input
+x=int(os.sys.argv[1])
+y=int(os.sys.argv[2])
+
+#ouput
+print ("Despues de resolver la inecuacion")
+print("Dar como respuesta el intervalo")
+print("Rpta: ")
+
+#iterador_rang
+numeros = list(range(x,y))
+print(numeros)
+
+#fin_iterador_rang
