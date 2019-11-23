ed70645974842fd19db4e2351c386f8d382c3868
miguel aguilar <maguilarv@unprg.edu.pe>
2019-11-22 20:52:08 -0500

    se agrego el lote.bat con textos validos sen ha unido el archivo de mi compañero y el mio juntos

---
+++ lote.bat
@@ -0,0 +1,87 @@
+python aguilar/mientras/ejercicio01.py "Miguel" "reunion familiar" domingo
+
+python aguilar/mientras/ejercicio02.py "Mauricio" "ing. electronica" 50
+
+python aguilar/mientras/ejercicio03.py "Juan" "manzanas" 10 4.5
+
+python aguilar/mientras/ejercicio04.py "miguel_av" "maguilarv@unprg.edu.pe" "12344" 18
+
+python aguilar/mientras/ejercicio05.py "Coraima" "Miguel" noviembre "quisieras salir conmigo?"
+
+python aguilar/para/ejercicio06.py 30
+
+python aguilar/para/ejercicio07.py 25
+
+python aguilar/para/ejercicio08.py 17
+
+python aguilar/para/ejercicio09.py 19
+
+python aguilar/para/ejercicio10.py 80
+
+python aguilar/iteracion/ejercicio11.py "AEIO"
+
+python aguilar/iteracion/ejercicio12.py "AMOR"
+
+python aguilar/iteracion/ejercicio13.py "VXY"
+
+python aguilar/iteracion/ejercicio14.py "COLMENA"
+
+python aguilar/iteracion/ejercicio15.py "CORA"
+
+python aguilar/iteracion_en_rango/ejercicio16.py 2 20 2
+
+python aguilar/iteracion_en_rango/ejercicio17.py 1 35
+
+python aguilar/iteracion_en_rango/ejercicio18.py 3 56 4
+
+python aguilar/iteracion_en_rango/ejercicio19.py 4 21
+
+python aguilar/iteracion_en_rango/ejercicio20.py 3 29
+
+python arroyo/mientras1.py "Gonzalo" "charla educativa" viernes
+
+python arroyo/mientras2.py "Manuel" "COMERCIAL DE TODO" 50
+
+python arroyo/mientras3.py "Giulio" "mandarinas" 40 5.6
+
+python arroyo/mientras4.py "Diana" "Sanchez" "Cutervo" 34
+
+python arroyo/mientras5.py "Fernando" "Joseph" julio "espero que vengas"
+
+python arroyo/iteration1.py "OIAEU"
+
+python arroyo/iteration1.py "PEDRO"
+
+python arroyo/iteration1.py "MARCO"
+
+python arroyo/iteration1.py "SOCFBI"
+
+python arroyo/iteration1.py "CAFETA"
+
+python arroyo/iteration_en_rango1.py 4 18
+
+python arroyo/iteration_en_rango1.py 3 40 3
+
+python arroyo/iteration_en_rango1.py 5 45
+
+python arroyo/iteration_en_rango1.py 3 32
+
+python arroyo/iteration_en_rango1.py 2 54 4
+
+python arroyo/para1.py 50
+
+python arroyo/para1.py 200
+
+python arroyo/para1.py 100
+
+python arroyo/para1.py 20
+
+python arroyo/para1.py 20
+
+
+
+
+
+
+
+
